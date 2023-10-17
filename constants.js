1c65b16cbff37eabc8a41322243a894c8d23870b
Eric Brelsford <ebrelsford@gmail.com>
2023-10-16 23:00:54 -0400

    Zoom out by default

--- constants.js
+++ constants.js
@@ -12,6 +12,6 @@ export const AIRTABLE_TEXT_BLOCKS_TABLE = 'text blocks';
 export const MAPBOX_STYLE = 'mapbox://styles/housingjusticelab2019/clga8nlf200cv01mksssposgz';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaG91c2luZ2p1c3RpY2VsYWIyMDE5IiwiYSI6ImNsZnZsd3JpMjAxcjEzY3A5cWZscHlsOWsifQ.U8sFEzf1nQJREh1_-XQ8jA';
 
-export const INITIAL_ZOOM = 12;
+export const INITIAL_ZOOM = 10;
 export const INITIAL_CENTER = { lng: -73.990, lat: 40.693 };
 export const MAXIMUM_EXTENT = [-74.464, 40.368, -73.453, 41.065];
