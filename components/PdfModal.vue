2d5cae8342f83ce018f06aa6a21512a45e01f85e
Eric Brelsford <ebrelsford@gmail.com>
2024-12-30 12:24:25 -0500

    Show PDFs in a modal

---
+++ components/PdfModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <modal>
+    <div class="pdf-modal">
+      <object
+        width="100%"
+        height="100%"
+        type="application/pdf" 
+        :data="url"
+        :title="title"
+      >
+        <p>PDF cannot be displayed.</p>
+      </object>
+    </div>
+  </modal>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  url: String,
+});
+
+const { title, url } = toRefs(props);
+</script>
+
+<style scoped>
+.pdf-modal {
+  flex-grow: 1;
+}
+</style>
