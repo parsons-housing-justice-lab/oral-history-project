233b94a9a22717d6e6ded1fa71bc626e329d795e
Eric Brelsford <ebrelsford@gmail.com>
2025-05-04 10:34:50 -0400

    Add images

--- scripts/fetch-airtable.js
+++ scripts/fetch-airtable.js
@@ -12,18 +12,33 @@ import {
 
 
 const baseOutputDir = 'public/content';
+const baseOutputImageDir = `${baseOutputDir}/images`;
 
 const loadTable = async (fetchFn, tableName, mapFn = d => d) => {
   const records = await fetchFn();
-  const mapped = records.map(mapFn);
+  const mapped = await Promise.all(records.map(mapFn));
   await fs.writeFile(`${baseOutputDir}/${tableName}.json`, JSON.stringify(mapped));
 };
 
-const mapProject = p => ({
-  ...p,
-  Photo: p.Photo[0].url,
-  RecordId: p.id,
-});
+const fetchImage = async (url, table, id) => {
+  const imageResponse = await fetch(url);
+  const ext = imageResponse.headers.get('content-disposition')
+    .split(".").slice(-1)[0].replace('"', '');
+  const arrayBuffer = await imageResponse.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const filePath = `images/${table}/${id}.${ext}`;
+  await fs.writeFile(`${baseOutputDir}/${filePath}`, buffer);
+  return `/content/${filePath}`;
+};
+
+const mapProject = async p => {
+  const photoUrl = await fetchImage(p.Photo[0].url, 'projects', p.id);
+  return {
+    ...p,
+    Photo: photoUrl,
+    RecordId: p.id,
+  };
+};
 
 const mapInterview = i => ({
   ...i,
